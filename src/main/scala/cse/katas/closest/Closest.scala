1c51e3b0aa8ab93c077e54a8d5197c918da6031b
Dan Wiebe <dnwiebe@gmail.com>
2016-09-04 10:20:30 -0400

    Closer negative value test passes

--- src/main/scala/cse/katas/closest/Closest.scala
+++ src/main/scala/cse/katas/closest/Closest.scala
@@ -6,12 +6,15 @@ package cse.katas.closest
 object Closest {
 
   val closestToZero: List[Int] => Option[Int] = numbers => {
-    if (numbers.isEmpty) {None}
-    else {
-      val negatives = numbers.filter {n => n < 0}
-      val positives = numbers.filter {n => n >= 0}
-      if (positives.nonEmpty) {Some (positives.min)}
-      else {Some (negatives.max)}
+    val negatives = numbers.filter {n => n < 0}
+    val positives = numbers.filter {n => n >= 0}
+    val minPositive = if (positives.nonEmpty) {Some (positives.min)} else {None}
+    val maxNegative = if (negatives.nonEmpty) {Some (negatives.max)} else {None}
+    (minPositive, maxNegative) match {
+      case (None, None) => None
+      case (p, None) => p
+      case (None, n) => n
+      case (Some (p), Some (n)) => if (p < -n) {Some (p)} else {Some (n)}
     }
   }
 }
