330f6e70d8158f93c102ec6e7617e04330f56d60
Dan Wiebe <dnwiebe@gmail.com>
2016-09-04 10:40:26 -0400

    Refactoring production code

--- src/main/scala/cse/katas/closest/Closest.scala
+++ src/main/scala/cse/katas/closest/Closest.scala
@@ -1,16 +1,27 @@
 package cse.katas.closest
 
+import scala.collection.TraversableOnce
+
 /**
   * Created by dnwiebe on 9/4/16.
   */
 object Closest {
 
+  private val findExtreme = (numbers: List[Int], discriminator: Int => Boolean, extreminator: List[Int] => Int) => {
+    val filtered = numbers.filter (discriminator)
+    if (filtered.nonEmpty) {Some (extreminator (filtered))} else {None}
+  }
+
+  private val findMinPositive: List[Int] => Option[Int] = numbers => {
+    findExtreme (numbers, {n => n >= 0}, {x => x.min})
+  }
+
+  private val findMinNegative: List[Int] => Option[Int] = numbers => {
+    findExtreme (numbers, {n => n < 0}, {x => x.max})
+  }
+
   val closestToZero: List[Int] => Option[Int] = numbers => {
-    val negatives = numbers.filter {n => n < 0}
-    val positives = numbers.filter {n => n >= 0}
-    val minPositive = if (positives.nonEmpty) {Some (positives.min)} else {None}
-    val maxNegative = if (negatives.nonEmpty) {Some (negatives.max)} else {None}
-    (minPositive, maxNegative) match {
+    (findMinPositive (numbers), findMinNegative (numbers)) match {
       case (None, None) => None
       case (p, None) => p
       case (None, n) => n
