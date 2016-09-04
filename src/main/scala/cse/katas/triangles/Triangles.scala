9403ce615203302e77c13f14dbd7fbf48cf25482
Dan Wiebe <dnwiebe@gmail.com>
2016-09-04 08:48:09 -0400

    Equilateral and Isosceles done

--- src/main/scala/cse/katas/triangles/Triangles.scala
+++ src/main/scala/cse/katas/triangles/Triangles.scala
@@ -4,15 +4,39 @@ package cse.katas.triangles
   * Created by dnwiebe on 9/4/16.
   */
 
-trait Classification
-case object Equilateral extends Classification
-case object Isosceles extends Classification
-case object Right extends Classification
-case object Other extends Classification
-case object NotATriangle extends Classification
+trait Classification {
+  def qualifies (a: Int, b: Int, c: Int): Boolean
+
+  private val ORDERS = List ((0, 1, 2), (1, 2, 0), (2, 0, 1), (2, 1, 0), (1, 0, 2), (0, 2, 1))
+  protected def rotate (a: Int, b: Int, c: Int, closure: (Int, Int, Int) => Boolean): Boolean = {
+    val argArray = Array (a, b, c)
+    ORDERS.exists {order => closure (argArray (order._1), argArray (order._2), argArray (order._3))}
+  }
+}
+
+case object Equilateral extends Classification {
+  override def qualifies (a: Int, b: Int, c: Int): Boolean = rotate (a, b, c, {(a, b, c) => (a == b) && (b == c)})
+}
+case object Isosceles extends Classification {
+  override def qualifies (a: Int, b: Int, c: Int): Boolean = rotate (a, b, c, {(a, b, c) => a == b})
+}
+case object Right extends Classification {
+  override def qualifies (a: Int, b: Int, c: Int): Boolean = false
+}
+case object Other extends Classification {
+  override def qualifies (a: Int, b: Int, c: Int): Boolean = false
+}
+case object NotATriangle extends Classification {
+  override def qualifies (a: Int, b: Int, c: Int): Boolean = false
+}
 
 object Triangles {
+  private val CLASSIFICATION_SEQUENCE = List (Equilateral, Isosceles, Right, Other)
+
   def classifySegments (a: Int, b: Int, c: Int): Classification = {
-    NotATriangle
+    CLASSIFICATION_SEQUENCE.find {triangleType => triangleType.qualifies (a, b, c)} match {
+      case Some (triangleType) => triangleType
+      case None => NotATriangle
+    }
   }
 }
--- src/test/scala/cse/katas/triangles/TrianglesTests.scala
+++ src/test/scala/cse/katas/triangles/TrianglesTests.scala
@@ -6,11 +6,29 @@ import org.scalatest.path
   * Created by dnwiebe on 9/4/16.
   */
 class TrianglesTests extends path.FunSpec {
-  describe ("Three equal segments") {
-    val result = Triangles.classifySegments (5, 5, 5)
 
-    it ("make an equilateral triangle") {
-      assert (result === Equilateral)
+  inOneOrder ("Three equal segments", 5, 5, 5, Equilateral)
+
+  describe ("Two equal segments and one unequal one") {
+    inAllOrders (5, 5, 4, Isosceles)
+  }
+
+  private def inOneOrder (msg: String, a: Int, b: Int, c: Int, expected: Classification): Unit = {
+    describe (s"${msg} (${a}, ${b}, ${c})") {
+      val result = Triangles.classifySegments (a, b, c)
+
+      it (s"classify as ${expected.getClass.getSimpleName.reverse.substring (1).reverse}") {
+        assert (result === expected)
+      }
     }
+  }
+
+  private def inAllOrders (a: Int, b: Int, c: Int, expected: Classification): Unit = {
+    inOneOrder ("in specified order", a, b, c, expected)
+    inOneOrder ("rotated once", b, c, a, expected)
+    inOneOrder ("rotated twice", c, a, b, expected)
+    inOneOrder ("reversed", c, b, a, expected)
+    inOneOrder ("reversed and rotated once", b, a, c, expected)
+    inOneOrder ("reversed and rotated twice", a, c, b, expected)
   }
 }
