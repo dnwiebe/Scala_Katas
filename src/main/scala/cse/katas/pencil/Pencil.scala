e0172a27489308afd17bd4c93434af59f5ed17e4
Dan Wiebe <dnwiebe@gmail.com>
2017-02-20 14:25:24 -0500

    First three steps in the can

--- src/main/scala/cse/katas/pencil/Pencil.scala
+++ src/main/scala/cse/katas/pencil/Pencil.scala
@@ -3,9 +3,28 @@ package cse.katas.pencil
 /**
   * Created by dnwiebe on 2/20/17.
   */
-class Pencil {
+class Pencil (val lifespan: Int, val sharpenings: Int) {
+
+  private var pointsRemaining = 0
+  private var sharpeningsRemaining = sharpenings + 1
+
+  sharpen ()
 
   def write (input: String): String = {
-    null
+    val (newRemaining, output) = input.foldLeft ((pointsRemaining, "")) {(soFar, c) =>
+      soFar match {
+        case (remaining, output) if remaining <= 0 => (remaining, output + " ")
+        case (remaining, output) => (remaining - 100, output + c)
+      }
+    }
+    pointsRemaining = newRemaining;
+    output
+  }
+
+  def sharpen (): Unit = {
+    if (sharpeningsRemaining > 0) {
+      pointsRemaining = lifespan * 100
+      sharpeningsRemaining -= 1;
+    }
   }
 }
--- src/test/scala/cse/katas/pencil/PencilTests.scala
+++ src/test/scala/cse/katas/pencil/PencilTests.scala
@@ -9,14 +9,56 @@ import org.scalatest.path
  */
 class PencilTests extends path.FunSpec {
 
-  describe ("A Pencil") {
-    val subject = new Pencil ()
+  describe ("A Pencil with a 40-character lifespan and two available sharpenings") {
+    val subject = new Pencil (40, 2)
 
-    describe ("directed to write a string") {
+    describe ("directed to write a string less than 40 characters") {
       val result = subject.write ("Fourscore and seven years ago");
 
-      it ("does so") {
+      it ("writes the whole thing") {
         assert (result === "Fourscore and seven years ago")
+      }
+
+      describe ("and then directed to write some more") {
+        val result = subject.write ("our fathers brought forth on this continent")
+
+        it ("craps out after 40 total characters") {
+          assert (result === "our fathers                                ")
+        }
+
+        describe ("and then sharpened") {
+          subject.sharpen ()
+
+          describe ("and directed to write more") {
+            val result = subject.write ("brought forth on this continent a new nation")
+
+            it ("writes 40 more characters and stops") {
+              assert (result === "brought forth on this continent a new na    ")
+            }
+
+            describe ("and sharpened again and used to write even more, then sharpened a third time") {
+              subject.sharpen ()
+              subject.write ("tion, conceived in liberty and dedicated to the proposition")
+              subject.sharpen ()
+
+              describe ("and used again") {
+                val result = subject.write ("to the proposition that all men are created equal")
+
+                it ("writes nothing this time") {
+                  assert (result === "                                                 ")
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+
+    describe ("directed to write a string longer than 40 characters") {
+      val result = subject.write ("Fourscore and seven years ago our fathers brought forth on this continent")
+
+      it ("only writes part of it") {
+        assert (result === "Fourscore and seven years ago our father                                 ")
       }
     }
   }
