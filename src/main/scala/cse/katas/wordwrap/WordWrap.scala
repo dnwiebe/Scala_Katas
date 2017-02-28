b2f8439ee8d2a017b312677be343dd435c13921d
Dan Wiebe <dnwiebe@gmail.com>
2017-02-28 12:53:18 -0500

    With Gabriel Sheeley's corrections

--- src/main/scala/cse/katas/wordwrap/WordWrap.scala
+++ src/main/scala/cse/katas/wordwrap/WordWrap.scala
@@ -7,8 +7,8 @@ object WordWrap {
 
   def wrap (input: String, lineLength: Int): String = {
     input match {
-      case s if s.length () <= lineLength => input
-      case s => input.lastIndexOf (' ', lineLength) match {
+      case s if s.length <= lineLength => s
+      case s => s.lastIndexOf (' ', lineLength) match {
         case idx if idx < 0 => s.substring (0, lineLength) + "\n" + wrap (s.substring (lineLength), lineLength)
         case idx => s.substring (0, idx) + "\n" + wrap (s.substring (idx + 1), lineLength)
       }
