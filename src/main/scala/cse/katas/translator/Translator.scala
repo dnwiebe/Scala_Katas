68a523bdf3f4e65595175ea09d6777b12f07262a
Dan Wiebe <dnwiebe@gmail.com>
2017-09-01 20:14:17 -0400

    Works with a few tests

--- src/main/scala/cse/katas/translator/Translator.scala
+++ src/main/scala/cse/katas/translator/Translator.scala
@@ -4,8 +4,36 @@ package cse.katas.translator
   * Created by dnwiebe on 8/30/17.
   */
 
+object Translator {
+  val twoNones: List[Option[Char]] = None :: None :: Nil
+}
+
 class Translator {
+  import Translator._
+
   def translate (camelCase: String): String = {
-    null
+    if (camelCase.isEmpty ()) {return ""}
+    val withEdgeEffects = (camelCase + ' ').foldLeft ((' ', ' ', "")) {(soFar, c) =>
+      val (a, b, snakeCase) = soFar
+      (a.isUpper, b.isUpper, c.isUpper) match {
+        case (false, false, false) => withB (snakeCase, b, c)
+        case (false, false, true) => withB (snakeCase, b, c)
+        case (true, false, false) => withB (snakeCase, b, c)
+        case (true, false, true) => withB (snakeCase, b, c)
+        case (false, true, false) => with_B (snakeCase, b, c)
+        case (false, true, true) => with_B (snakeCase, b, c)
+        case (true, true, false) => with_B (snakeCase, b, c)
+        case (true, true, true) => withB (snakeCase, b, c)
+      }
+    }._3
+    withEdgeEffects.drop (2)
+  }
+
+  private def withB (snakeCase: String, b: Char, c: Char): (Char, Char, String) = {
+    (b, c, snakeCase + b.toLower)
+  }
+
+  private def with_B (snakeCase: String, b: Char, c: Char): (Char, Char, String) = {
+    (b, c, snakeCase + '_' + b.toLower)
   }
 }
--- src/test/scala/cse/katas/translator/TranslatorTests.scala
+++ src/test/scala/cse/katas/translator/TranslatorTests.scala
@@ -21,5 +21,21 @@ class TranslatorTests extends path.FunSpec {
         assert (result === "")
       }
     }
+
+    describe ("directed to translate a simple capitalized string") {
+      val result = subject.translate ("Simple")
+
+      it ("produces a lowercased version") {
+        assert (result === "simple")
+      }
+    }
+
+    describe ("directed to translate a camelcased string with an abbreviation") {
+      val result = subject.translate ("ICalledTheAAAWhenMyCarBrokeDown")
+
+      it ("does so") {
+        assert (result === "i_called_the_aaa_when_my_car_broke_down")
+      }
+    }
   }
 }
