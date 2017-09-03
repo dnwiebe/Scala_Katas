47ee00cb8b197f39577a3dba65074584b24e8907
Dan Wiebe <dnwiebe@gmail.com>
2017-09-03 17:30:53 -0400

    Inlined a couple of once-called functions

--- src/main/scala/cse/katas/closest/Closest.scala
+++ src/main/scala/cse/katas/closest/Closest.scala
@@ -5,28 +5,20 @@ package cse.katas.closest
   */
 object Closest {
 
+  def closestToZero (numbers: List[Int]): Option[Int] = {
+    (findExtremeFiltered (numbers, _ >= 0, _.min), findExtremeFiltered (numbers, _ < 0, _.max)) match {
+      case (None, None) => None
+      case (p, None) => p
+      case (None, n) => n
+      case (Some (p), Some (n)) => if (-n < p) {Some (n)} else {Some (p)}
+    }
+  }
+
   private def findExtremeFiltered (numbers: List[Int], discriminator: Int => Boolean,
                                    extreminator: List[Int] => Int): Option[Int] = {
     numbers.filter (discriminator) match {
       case Nil => None
       case filtered => Some (extreminator (filtered))
-    }
-  }
-
-  private def findMinPositive (numbers: List[Int]): Option[Int] = {
-    findExtremeFiltered (numbers, _ >= 0, _.min)
-  }
-
-  private def findMinNegative (numbers: List[Int]): Option[Int] = {
-    findExtremeFiltered (numbers, _ < 0, _.max)
-  }
-
-  def closestToZero (numbers: List[Int]): Option[Int] = {
-    (findMinPositive (numbers), findMinNegative (numbers)) match {
-      case (None, None) => None
-      case (p, None) => p
-      case (None, n) => n
-      case (Some (p), Some (n)) => if (-n < p) {Some (n)} else {Some (p)}
     }
   }
 }
